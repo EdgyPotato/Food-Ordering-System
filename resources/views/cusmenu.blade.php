7173dcb21cd37a2fc37fa9be6fc12ccde14d5270
Heng1007 <bigheadson1007@gmail.com>
2023-11-03 14:56:07 +0800

    Customer Menu (half)

--- resources/views/cusmenu.blade.php
+++ resources/views/cusmenu.blade.php
@@ -1,9 +1,28 @@
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<!DOCTYPE html>
+<html>
 
-<div class="main">1
-    <div>1a
-        <div>1aa</div>
-    </div>
-    <div>1b</div>
-</div>
-<div>2</div>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/cusmenu.css') }}">
+    </head>
+
+    <body>
+        <div class="purplearea">
+            <div class="firstrow">
+                <div class="tableNo">TABLE NO</div>
+                <div class="login"><a href=login1>LOGIN</a></div>
+            </div>
+            <div class="searchbar">
+                <i><img src="{{ asset('image/search.png')}}" width="20px" height="22px"></i>
+                <input type="text">
+            </div>
+            <div class="categories">
+                <div id="c1">Food</div>
+                <div id="c2">Beverages</div>  
+                <div id="c3">Others</div>              
+            </div>
+        </div>
+    </body>
+</html>
+
+
--- resources/views/login1.blade.php
+++ resources/views/login1.blade.php
@@ -21,7 +21,7 @@
   ```
 -->
 
-
+<!DOCTYPE html>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="{{ asset('css/style.css')}}">
@@ -31,10 +31,10 @@
     <div class="return"><a href=cusmenu><img src="{{ asset('image/return.png')}}" width="30px" height="30px"></a></div>
   </div>
   <div class="content">
-    <div class="empty"></div>
+    <div class="empp"></div>
     <div class="upperfont">
-      <p id="p1">Login</p>
-      <p id="p2">Please sign in to continue</p>
+      <p id="login">Login</p>
+      <p id="signin">Please sign in to continue</p>
     </div>
     <div class="empty2"></div>
     <div class="form">
