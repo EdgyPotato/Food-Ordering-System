b1a14e9ffe5e16fc53f48371ea78524cc302cc95
Heng1007 <bigheadson1007@gmail.com>
2023-11-04 23:13:45 +0800

    link

--- resources/views/cusmenu.blade.php
+++ resources/views/cusmenu.blade.php
@@ -17,22 +17,44 @@
                 <input type="text">
             </div>
             <div class="categories">
-                <div id="c1">Food</a></div>
-                <div id="c2">Beverages</div>  
+                <div id="c1"><a href="#food">Food</a></div>
+                <div id="c2"><a href="#beverage">Beverages</a></div>  
                 <div id="c3">Others</div>              
             </div>
         </div>
         <div class="emptyarea"></div>
         <div class="whitearea">
         <?php
-            for ($i = 0; $i < 10; $i++) {
-                echo "<div id='categories'>Food</div>";
+            $sql = DB::table('menu')
+            ->where('categories', '=', 'food')
+            ->get();
+            echo "<div id='food'>Food</div>"; //food subtitle
+            foreach ($sql as $food){
                 echo "<div class='box'>";
                     echo "<div class='picture'>picture</div>";
                     echo "<div class='nopicture'>";
-                        echo "<div class='food'>food name</div>";
-                        echo "<div class='description'>description</div>";
-                        echo "<div class='price'>price</div>";
+                        echo "<div class='foodandpricebox'>";
+                            echo "<div class='food'>" . $food->id . '. ' . $food->foodname ."</div>";
+                            echo "<div class='price'>". "RM " . $food->price."</div>";
+                        echo "</div>";
+                        echo "<div class='description'>". $food->description ."</div>";
+                    echo "</div>";
+                echo "</div>";
+            }
+            echo "</br>";
+            $sql = DB::table('menu') //get the beverage list
+            ->where('categories', '=', 'beverages')
+            ->get();
+            echo "<div id='beverage'>Beverages</div>"; //beverage subtitle
+            foreach ($sql as $food){
+                echo "<div class='box'>";
+                    echo "<div class='picture'>picture</div>";
+                    echo "<div class='nopicture'>";
+                        echo "<div class='foodandpricebox'>";
+                            echo "<div class='food'>" . $food->id . '. ' . $food->foodname ."</div>";
+                            echo "<div class='price'>". "RM " . $food->price."</div>";
+                        echo "</div>";
+                        echo "<div class='description'>". $food->description ."</div>";
                     echo "</div>";
                 echo "</div>";
             }
@@ -48,6 +70,16 @@
             <div class="add"><a href="viewcart"><img src="{{ asset('image/cart.png')}}" width="40px" height="40px"></a></div>
         </div>
         <div class="emptyarea2"></div>
+
+        <script>
+        function scrollToSubtitle() {
+            const yOffset = -50; // Adjust this value based on the height of your fixed top bar
+            const element = document.getElementById('food');
+            const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+        </script>
     </body>
     
 </html>
