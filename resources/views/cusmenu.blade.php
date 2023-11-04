f77664d471be91f91f77399f06bbade39ca077e6
Heng1007 <bigheadson1007@gmail.com>
2023-11-05 00:09:28 +0800

    try

--- resources/views/cusmenu.blade.php
+++ resources/views/cusmenu.blade.php
@@ -72,14 +72,14 @@
 
         <script>
         function scrollToSubtitle() {
-            const yOffset = -50; // Adjust this value based on the height of your fixed top bar
+            const yOffset = -50; // Adjust this value based on the height of your fixed top bar >>>>
             const element = document.getElementById('food');
             const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
             window.scrollTo({ top: y, behavior: 'smooth' });
         }
         </script>
-    </body>
+    </body> 
     
 </html>
 
