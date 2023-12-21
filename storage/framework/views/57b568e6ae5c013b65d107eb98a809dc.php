4e97fe4115be1d58f4ef2b1289d8806d41e6e9ea
Heng1007 <bigheadson1007@gmail.com>
2023-12-21 19:45:27 +0800

    dashboard from kaiyuan

--- storage/framework/views/57b568e6ae5c013b65d107eb98a809dc.php
+++ storage/framework/views/57b568e6ae5c013b65d107eb98a809dc.php
@@ -82,7 +82,87 @@
                     </li>
                 </ol>
             </nav>
-            <!-- write here -->
+            <main class="flex flex-col w-full h-full">
+            <div class="flex" id="top-3">
+                <div class="flex flex-row justify-start items-center mt-8 px-6 gap-6 w-full h-full">
+                    <div class="flex flex-col px-6 py-6 bg-white shadow-md w-1/3 rounded-xl">
+                        <div class="w-full flex justify-between items-center">
+                            <h1 class="text-2xl font-bold">Sales</h1>
+                            <button type="button" id="saleButton" data-dropdown-toggle="dropdownSale">
+                                <svg class="w-8 h-8 fill-grey-400" viewBox="0 0 32 32">
+                                    <circle cx="16" cy="16" r="2"></circle>
+                                    <circle cx="10" cy="16" r="2"></circle>
+                                    <circle cx="22" cy="16" r="2"></circle>
+                                </svg>
+                            </button>
+                            <div id="dropdownSale" class="z-10 hidden bg-grey-100 rounded-lg shadow">
+                                <a href="/dashboard/sales" class="block px-4 py-2 hover:bg-grey-200 hover:rounded-lg ">View More</a>
+                            </div>
+                        </div>
+                        <p class="text-base text-grey-600">Total sales of this month</p>
+                        <h1 class="text-3xl mt-2 font-bold">$10000.00</h1>
+                    </div>
+
+                    <div class="flex flex-col px-6 py-6 bg-white shadow-md w-1/3 rounded-xl">
+                        <div class="w-full flex justify-between items-center">
+                            <h1 class="text-2xl font-bold">Expense</h1>
+                            <button type="button" id="expenseButton" data-dropdown-toggle="dropdownExpense">
+                                <svg class="w-8 h-8 fill-grey-400" viewBox="0 0 32 32">
+                                    <circle cx="16" cy="16" r="2"></circle>
+                                    <circle cx="10" cy="16" r="2"></circle>
+                                    <circle cx="22" cy="16" r="2"></circle>
+                                </svg>
+                            </button>
+                            <div id="dropdownExpense" class="z-10 hidden bg-grey-100 rounded-lg shadow">
+                                <a href="/dashboard/expense" class="block px-4 py-2 hover:bg-grey-200 hover:rounded-lg ">View More</a>
+                            </div>
+                        </div>
+                        <p class="text-base text-grey-600">Total expense of this month</p>
+                        <h1 class="text-3xl mt-2 font-bold">$4000.00</h1>
+                    </div>
+
+                    <div class="flex flex-col px-6 py-6 bg-white shadow-md w-1/3 rounded-xl">
+                        <div class="w-full flex justify-between items-center">
+                            <h1 class="text-2xl font-bold">Profit</h1>
+                            <button type="button" id="profitButton" data-dropdown-toggle="dropdownProfit">
+                                <svg class="w-8 h-8 fill-grey-400" viewBox="0 0 32 32">
+                                    <circle cx="16" cy="16" r="2"></circle>
+                                    <circle cx="10" cy="16" r="2"></circle>
+                                    <circle cx="22" cy="16" r="2"></circle>
+                                </svg>
+                            </button>
+                            <div id="dropdownProfit" class="z-10 hidden bg-grey-100 rounded-lg shadow">
+                                <a href="/dashboard/profit" class="block px-4 py-2 hover:bg-grey-200 hover:rounded-lg ">View More</a>
+                            </div>
+                        </div>
+                        <p class="text-base text-grey-600">Total profit of this month</p>
+                        <h1 class="text-3xl mt-2 font-bold">$6000.00</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex px-6 mt-12" id="chart">
+                <div class="flex flex-col px-6 py-6 bg-white shadow-md w-full rounded-xl">
+                    <div class="w-full flex justify-between items-center">
+                        <h1 class="text-2xl font-bold">Profit of Months</h1>
+                        <button type="button" id="profitMonthButton" data-dropdown-toggle="dropdownProfitMonth">
+                            <svg class="w-8 h-8 fill-grey-400" viewBox="0 0 50 50">
+                                <circle cx="20" cy="20" r="5"></circle>
+                                <circle cx="5" cy="20" r="5"></circle>
+                                <circle cx="35" cy="20" r="5"></circle>
+                            </svg>
+                        </button>
+                        <div id="dropdownProfitMonth" class="z-10 hidden bg-grey-100 rounded-lg shadow">
+                            <a href="#" class="block px-4 py-2 hover:bg-grey-200 hover:rounded-lg ">View More</a>
+                        </div>
+                    </div>
+                    <p class="mb-2 text-base text-grey-600">Total profit of the months</p>
+                    <div class="flex-grow">
+                        <canvas id="dashboard-card-01" width="848" height="400"></canvas>
+                    </div>
+                </div>
+            </div>
+        </main>
         </main>
 </body>
 </html>
