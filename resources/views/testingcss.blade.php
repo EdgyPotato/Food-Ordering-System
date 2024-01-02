04ce356f6b36a082047fd25f5dc19aa5f828033a
DirtBean <tanyuan@graduate.utm.my>
2024-01-02 13:45:29 +0800

    remove css stylesheets

--- resources/views/testingcss.blade.php
+++ resources/views/testingcss.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <style>
         @media screen and (orientation: portrait) {
             html {
