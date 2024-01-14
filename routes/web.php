18825086e084e3b25fa46e0838094535a5e8b454
DirtBean <tanyuan@graduate.utm.my>
2024-01-15 02:35:30 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::get('/logout', function () {
 
 //Customer (Not Need User Level) need table session
 Route::get('/', function () {
-    $table = request()->input('table');
+    $table = request()->get('table');
 
     if ($table) {
         // Perform any necessary validation or processing on $table
